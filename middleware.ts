aeb8f63e5c4e3e9683457e68f699b4f1a7393de6
Srđan Maljukan <srdjanmaljukan96@gmail.com>
2026-04-11 11:30:48 +0200

    Implement middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,21 @@
+import { auth } from "./auth";
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+  const isSignInPage = req.nextUrl.pathname === "/sign-in";
+
+  if (!isLoggedIn && !isSignInPage) {
+    return NextResponse.redirect(new URL("/sign-in", req.nextUrl));
+  }
+
+  if (isLoggedIn && isSignInPage) {
+    return NextResponse.redirect(new URL("/", req.nextUrl));
+  }
+
   return NextResponse.next();
-}
+});
 
 export const config = {
-  matcher: [],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|api/auth).*)"],
 };
